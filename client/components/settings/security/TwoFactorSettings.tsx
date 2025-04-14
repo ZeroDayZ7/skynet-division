b3ee2660bdf0156a7e9509e51149027525423329
ZeroDayZ7 <ZeroDayZ7@proton.me>
2025-04-14 19:00:29 +0200

    READY OK

--- client/components/settings/security/TwoFactorSettings.tsx
+++ client/components/settings/security/TwoFactorSettings.tsx
@@ -6,6 +6,9 @@ import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { fetchClient } from '@/lib/fetchClient';
 import { toast } from 'sonner';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export function TwoFactorSettings() {
   const [twoFactorEnabled, setTwoFactorEnabled] = useState(false);
@@ -54,9 +57,57 @@ export function TwoFactorSettings() {
     <div className="flex items-center justify-between">
       <div>
         <Label htmlFor="2fa">Uwierzytelnianie dwuskładnikowe</Label>
-        <p className="text-sm text-muted-foreground">
-          Wymagaj kodu z aplikacji autoryzacyjnej przy logowaniu
-        </p>
+        <div className="flex items-center gap-2">
+        <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline">Open popover</Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-80">
+        <div className="grid gap-4">
+          <div className="space-y-2">
+            <h4 className="font-medium leading-none">Dimensions</h4>
+            <p className="text-sm text-muted-foreground">
+              Set the dimensions for the layer.
+            </p>
+          </div>
+          <div className="grid gap-2">
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="width">Width</Label>
+              <Input
+                id="width"
+                defaultValue="100%"
+                className="col-span-2 h-8"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="maxWidth">Max. width</Label>
+              <Input
+                id="maxWidth"
+                defaultValue="300px"
+                className="col-span-2 h-8"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="height">Height</Label>
+              <Input
+                id="height"
+                defaultValue="25px"
+                className="col-span-2 h-8"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <Label htmlFor="maxHeight">Max. height</Label>
+              <Input
+                id="maxHeight"
+                defaultValue="none"
+                className="col-span-2 h-8"
+              />
+            </div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+    </div>
       </div>
       <Switch
         id="2fa"
